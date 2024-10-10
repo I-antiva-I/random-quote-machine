650815d68104d6384bbd30917f312e6930fe96ca
I-antiva-I <a.a.ivanichenko@gmail.com>
2024-10-10 15:13:34 +0200

    Added use(settingsStore)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { settingsStore } from './scripts/settingsStore'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(settingsStore).mount('#app');
